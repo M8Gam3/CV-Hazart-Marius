cb0c20886d582092d3f52269a8b47e7466e4d478
Marius Hazart <115085694+M8Gam3@users.noreply.github.com>
2023-09-06 16:23:55 +0200

    push

--- new test/assets/js/script.js
+++ new test/assets/js/script.js
@@ -46,56 +46,374 @@
 //     }
 // }
 
-const btn_one = document.querySelector("#btn_one")
-    const menu_one = document.querySelector("#menu_one")
-    const section_one = document.querySelector("#section_one")
-        const home_icon = document.querySelector("#home_icon")
-        const home_content = document.querySelector("#home_content")
+Vue.component('object-category-skills', {
+    template:   `<div class="skill-object">
+                    <h4> {{name}} </h4>
+                    <progress v-bind:class="progresscolor(level)" id="file" max="100" v-bind:value="level * 20"></progress>
+                </div>`,
+    props: ['level', 'name', 'progresscolor'],
+});
 
-        const profile_icon = document.querySelector("#profile_icon")
-        const profile_content = document.querySelector("#profile_content")
+Vue.component('content-block0', {
+    template:   `<div class="content-block0"><slot></div>`,
+});
 
-        const skills_icon = document.querySelector("#skills_icon")
-        const skills_content = document.querySelector("#skills_content")
+Vue.component('content-block1', {
+    template:   `<div class="content-block1"><slot></div>`,
+});
+
+Vue.component('content-block2', {
+    template:   `<div class="content-block2"><slot></div>`,
+});
+
+Vue.component('content-block3', {
+    template:   `<div class="content-block3"><slot></div>`,
+});
+
+Vue.component('chat-content', {
+    template:   `<div class="chat-content"><slot></div>`,
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+
+        section_one: true,
+        home: true,
+        profil: false,
+        skills: false,
+
+        section_two: false,
+        district: true,
+        addressage: false,
+        shifumi: false,
+
+        skillsList : {
+            Languages: [
+                {
+                    name: 'HTML',
+                    level: 3,
+                    more:[
+                        
+                    ],
+                },
+                {
+                    name: 'CSS',
+                    level: 2,
+                    more:[
+                        {
+                            name: 'flex',
+                            level: 3,
+                        },
+                        {
+                            name: 'grid',
+                            level: 1
+                        },
+                        {
+                            name: 'reponsive',
+                            level: 2
+                        },
+                    ],
+                },
+                {
+                    name: 'Python',
+                    level: 4,
+                    more:[
+                        {
+                            name: 'optimisation',
+                            level: 4,
+                        },
+                    ],
+                },
+                {
+                    name: 'JavaScript',
+                    level: 4,
+                    more:[
+                        {
+                            name: 'optimisation',
+                            level: 4,
+                        },
+                        {
+                            name: 'local storage',
+                            level: 3,
+                        },
+                    ],
+                },
+                {
+                    name: 'PHP',
+                    level: 3.5,
+                    more:[
+                        {
+                            name: 'procédural',
+                            level: 4,
+                        },
+                        {
+                            name: 'optimisation',
+                            level: 3,
+                        },
+                        {
+                            name: 'GET/POST',
+                            level: 2,
+                        },
+                    ],
+                },
+                {
+                    name: 'mySQl',
+                    level: 3.20,
+                    more:[
+                        {
+                            name: 'SELECT',
+                            level: '3',
+                        },
+                    ],
+                },
+            ],
+            framework: [
+                {
+                    name: 'Vue.js',
+                    level: 4,
+                    more:[
+                        {
+                            name: 'optimisation',
+                            level: 3,
+                        },
+                        {
+                            name: 'templates',
+                            level: 4,
+                        },
+                    ],
+                },
+                {
+                    name: 'Bootstrap',
+                    level: 3,
+                    more:[
+                        {
+                            name: 'style',
+                            level: 3,
+                        },
+                    ],
+                },
+            ],
+            design: [
+                {
+                    name: 'Figma',
+                    level: 2,
+                    more:[
+                        {
+                            name: 'UI',
+                            level: 1,
+                        },
+                        {
+                            name: 'UX',
+                            level: 2,
+                        },
+                        {
+                            name: 'maquettage',
+                            level: 3,
+                        },
+                    ],
+                },
+                {
+                    name: 'Adobe XD',
+                    level: 1,
+                    more:[
+                        {
+                            name: 'UI',
+                            level: 1,
+                        },
+                        {
+                            name: 'UX',
+                            level: 2,
+                        },
+                        {
+                            name: 'maquettage',
+                            level: 2,
+                        },
+                    ],
+                },
+                {
+                    name: 'Photoshop',
+                    level: 1,
+                    more:[
+                        {
+                            name: 'UI',
+                            level: 1,
+                        },
+                        {
+                            name: 'UX',
+                            level: 2,
+                        },
+                        {
+                            name: 'modification d\'images',
+                            level: 1,
+                        },
+                    ],
+                },
+                {
+                    name: 'Adobe Illustrator',
+                    level: 1,
+                    more:[
+                        {
+                            name: 'création de logo',
+                            level: 1,
+                        },
+                        {
+                            name: 'création de bannière',
+                            level: 1,
+                        },
+                        {
+                            name: 'illustration',
+                            level: 1,
+                        },
+                    ],
+                },
+            ],
+            Autre: [
+                {
+                    name: 'Git',
+                    level: 3,
+                    more:[
+                        {
+                            name: 'Serveur GitHub',
+                            level: 4,
+                        },
+                        {
+                            name: 'Desktop',
+                            level: 4,
+                        },
+                        {
+                            name: 'Console',
+                            level: 1,
+                        },
+                        {
+                            name: 'Versionning',
+                            level: 3,
+                        },
+                        {
+                            name: 'Branch',
+                            level: 3,
+                        },
+                    ],
+                },
+                {
+                    name: 'Trello',
+                    level: 3,
+                    more:[
+                        {
+                            name: 'Lecture',
+                            level: '5',
+                        },
+                        {
+                            name: 'Gestion',
+                            level: '2',
+                        },
+                    ],
+                },
+                {
+                    name: 'XML',
+                    level: 3,
+                    more:[
+                        {
+                            name: 'RSS',
+                            level: 1,
+                        },
+                    ],
+                },
+            ],
+        },
+
+    },
+    computed: {
+
+    },
+    methods: {
+        navOne: function () {
+            this.section_one = true;
+            this.section_two = false;
+        },
+        navHome: function () {
+            this.home = true;
+            this.profil = false;
+            this.skills = false;
+        },
+        navProfil: function () {
+            this.home = false;
+            this.profil = true;
+            this.skills = false;
+        },
+        navSkills: function () {
+            this.home = false;
+            this.profil = false;
+            this.skills = true;
+        },
 
 
-const btn_two = document.querySelector("#btn_two")
-    const menu_two = document.querySelector("#menu_two")
-    const section_two = document.querySelector("#section_two")
+        navTwo: function () {
+            this.section_one = false;
+            this.section_two = true;
+        },
+        navDistrict: function () {
+            this.district = true;
+            this.addressage = false;
+            this.shifumi = false;
+        },
+        navAddressage: function () {
+            this.district = false;
+            this.addressage = true;
+            this.shifumi = false;
+        },
+        navShifumi: function () {
+            this.district = false;
+            this.addressage = false;
+            this.shifumi = true;
+        },
+
+
+        // navSkills: function () {
+        //     this.home = false;
+        //     this.portfolio = false;
+        //     this.presentation = false;
+        //     this.skills = true;
+        // },
 
 
 
-profile_content.classList.add("invisible");
-skills_content.classList.add("invisible");
-
-menu_two.classList.add("invisible");
-section_two.classList.add("invisible");
-
-btn_one.addEventListener("click", function(){
-    section_two.classList.add("invisible");
-    section_one.classList.remove("invisible");
-    menu_two.classList.add("invisible");
-    menu_one.classList.remove("invisible");
+        progressColor: function (level) {
+            if (level <= 1) {
+                return "progress-red";
+            } else if (level <= 2) {
+                return "progress-orange";
+            } else if (level <= 3) {
+                return "progress-yellow";
+            } else if (level <= 4) {
+                return "progress-green";
+            } else if (level <= 5) {
+                return "progress-succes";
+            }
+            // switch (level) {
+            //     case 1:
+            //         return "progress-red";
+            //     case 2:
+            //         return "progress-orange";
+            //     case 3:
+            //         return "progress-yellow";
+            //     case 4:
+            //         return "progress-green";
+            //     case 5:
+            //         return "progress-succes";
+            // };
+        },
+    },
 })
-    home_icon.addEventListener("click", function(){
-        profile_content.classList.add("invisible");
-        home_content.classList.remove("invisible");
-        skills_content.classList.add("invisible");
-    })
-    profile_icon.addEventListener("click", function(){
-        home_content.classList.add("invisible");
-        profile_content.classList.remove("invisible");
-        skills_content.classList.add("invisible");
-    })
-    skills_icon.addEventListener("click", function(){
-        home_content.classList.add("invisible");
-        profile_content.classList.add("invisible");
-        skills_content.classList.remove("invisible");
-    })
 
-btn_two.addEventListener("click", function(){
-    section_one.classList.add("invisible");
-    section_two.classList.remove("invisible");
-    menu_one.classList.add("invisible");
-    menu_two.classList.remove("invisible");
+let btnOne = document.querySelector("#btn_one")
+let btnTwo = document.querySelector("#btn_two")
+
+btnOne.addEventListener("click", function(){
+    btnOne.classList.add("logo");
+    btnTwo.classList.remove("logo");
+})
+btnTwo.addEventListener("click", function(){
+    btnOne.classList.remove("logo");
+    btnTwo.classList.add("logo");
 })
