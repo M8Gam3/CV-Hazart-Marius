06d749fa1e4a609f023bdb8b907b9d013588e7d5
Marius Hazart <115085694+M8Gam3@users.noreply.github.com>
2023-11-08 17:22:35 +0100

    push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,4 @@
-// const form = document.querySelector("form")
-// const chatMessages = document.querySelector(".chat__messages")
-// const input = document.querySelector(".sendMessage")
 
-// form.addEventListener("submit", sendMessage)
-
-// function sendMessage(e) {
-//     e.preventDefault()
-
-//     if(input.value !== "") {
-//         var messageDiv = document.createElement("div")
-//         messageDiv.className = "message"
-
-//         var avatar = document.createElement("img")
-//         avatar.src = "assets/user4.jpg"
-
-//         var messageInfo = document.createElement("div")
-//         messageInfo.className = "message__info"
-
-//         var userInfo = document.createElement("h4")
-//         userInfo.innerHTML = "Gamer"
-
-//         var messageTimestamp = document.createElement("span")
-//         messageTimestamp.className = "message__timestamp"
-
-//         const date = new Date()
-//         const year = date.getFullYear()
-//         const month = String(date.getMonth()).padStart(2, "0")
-//         const day = String(date.getDate()).padStart(2, "0")
-
-//         messageTimestamp.innerHTML = month + "/" + day + "/" + year
-
-//         const message = document.createElement("p")
-//         message.innerHTML = input.value
-//         input.value = ""
-
-//         userInfo.appendChild(messageTimestamp)
-//         messageInfo.appendChild(userInfo)
-//         messageInfo.appendChild(message)
-
-//         messageDiv.appendChild(avatar)
-//         messageDiv.appendChild(messageInfo)
-
-//         chatMessages.appendChild(messageDiv)
-//         chatMessages.scrollBy(0, 10000)
-//     }
-// }
 
 Vue.component('object-category-skills', {
     template:   `<div class="skill-object">
@@ -73,6 +27,45 @@ Vue.component('content-block3', {
 Vue.component('chat-content', {
     template:   `<div class="chat-content"><slot></div>`,
 });
+
+
+
+
+
+
+
+var touchRegionElement = document.querySelector('#app');
+var navbar = document.querySelector('#navbar');
+
+var initialX, initialY, initialTime;
+
+touchRegionElement.addEventListener('touchstart', function(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+    initialTime = new Date();
+});
+
+touchRegionElement.addEventListener('touchend', function(e) {
+    var deltaX = e.changedTouches[0].clientX - initialX;
+    var deltaY = Math.abs(e.changedTouches[0].clientY - initialY);
+    var deltaTime = new Date() - initialTime;
+
+    if (deltaX <= -30 && deltaY <= 100 && deltaTime <= 300) {
+        console.log('left');
+    }
+    else if (deltaX >= -30 && deltaY >= 100 && deltaTime <= 300) {
+        console.log('right');
+    }
+});
+
+
+
+
+
+
+
+
+
 
 var app = new Vue({
     el: '#app',
